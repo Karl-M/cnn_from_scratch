7602cd80a73859bff489e15f153e76d47c23f021
Konstantin <djdanger@gmx.de>
2019-08-07 17:06:21 +0200

    von Arbeit

--- cnn_from_scratch_blog.py
+++ cnn_from_scratch_blog.py
@@ -20,8 +20,8 @@ import sys
 #from pathlib import Path
 import mnist
 import os
-path = "/home/konstantin/Documents/master_arbeit/cnn_from_scratch"
-#path = "C:/Users/D2GU53/Documents/master_arbeit/nn_in_r"
+#path = "/home/konstantin/Documents/master_arbeit/cnn_from_scratch"
+path = "C:/Users/D2GU53/Documents/master_arbeit/nn_in_r"
 sys.path.append(path)
 import functions as fun
 
@@ -34,120 +34,23 @@ os.listdir(path)
 #sys.path.append(cats_and_dogs_folder)
 
 
-# generate some data
-image_size = 10
-
-sample_horizontal = []
-for i in range(1000):
-    X = np.zeros(shape=(image_size , image_size ))
-    a = np.random.choice(range(0, image_size ))
-    b = np.random.choice(range(0, image_size ))
-    X[a, ] = 1
-    X[b, ] = 2
-    sample_horizontal.append(X)
-
-sample_vertical = []
-for i in range(1000):
-    X = np.zeros(shape=(image_size , image_size ))
-    a = np.random.choice(range(0, image_size ))
-    b = np.random.choice(range(0, image_size ))
-    X[: , a] = 1
-    X[: , b] = 2
-    sample_vertical.append(X)
-
-
-
-data = [sample_horizontal, sample_vertical]
-
-test_features = fun.convolute(data[0][1], 5)
-test_features.shape
-
-
-fun.max_pool(test_features).shape
-    
-test_features.shape
-
-test_pool = fun.max_pool(test_features)
-
-
-## build fully connected layer for predition
-num_filter, height, width = test_pool.shape
-
-
-n_classes = 10
-
-weight_matrix = np.random.randn(num_filter * height * width, 10)
-
-
-
-
-test_conv = fun.convolute(data[1][12], 6)
-test_maxpool = fun.max_pool(test_conv)
-test_softmax = fun.softmax(test_maxpool, 10)
-
-test_softmax
-
 # why use softmax? To quantify how sure we are of our prediction
 # and use for example cross-entropy-loss
 
 # define forward pass through network
 
-test_images = mnist.test_images()[1000:2000]
-test_labels = mnist.test_labels()[1000:2000]
-
-import functions as fun
-
-n_classes = 10
-input_len = 1014
-weight_matrix = np.random.randn(input_len, n_classes) / input_len
-weight_matrix_old  = weight_matrix
-bias_vector = np.random.randn(n_classes) / n_classes
-bias_vector_old = bias_vector
-#bias_vector = bias_vector_old
-
-num_correct = 0
-for i in range(1000):
-    #false = 0
- #   print(f"bias_vector at iteration {i} : {bias_vector}")
-    # print(f"weight matrix at iteration {i}: {weight_matrix}")
-    result = fun.feed_forward(image=test_images[i], 
-                              label=test_labels[i], 
-                                 number_filters=6, 
-                                 n_classes=10,
-                                 weight=weight_matrix ,
-                                 bias=bias_vector,
-                                 learn_rate=0.01)
-    
-    weight_matrix = result[6]
-    bias_vector = result[7]
-#    print(weight_matrix)
-   # print(prediction, label, acc)
-    num_correct += result[2]
-   # print(num_correct)
-    if i % 100 == 0 and i != 0:
-        accuracy = num_correct / i
-        print(f"accuracy for the first {i} samples: {accuracy}")
-        print(f"{num_correct} predictions for {i} samples were correct")
-        print(f"bias_vector at iteration {i} : {bias_vector}")
-        print(f"weight matrix at iteration {i}: {weight_matrix}")
-    
-    #print(probabilities[label], prediction, label, acc)
-
-weight_matrix
-weight_matrix_old
-
-sum(weight_matrix == weight_matrix_old)
-
-bias_vector
-bias_vector_old
-result[6]
-# 10% accuracy is equivalent to random guessing, to do better, we need to train
-# the network. Training consists of two phases. A forward pass and a backward pass.
-
-test_images[1].shape
-
-np.prod(test_images.shape)
+test_images = mnist.test_images()[20:2000]
+test_labels = mnist.test_labels()[20:2000]
 
 
+conv, soft = fun.training(1001, 10, 8, test_images, test_labels, learn_rate=0.01, print_acc=True)
 
-test_images[1] / 255 -0.5
+conv, soft = fun.training(1001, 10, 8, test_images, test_labels, 
+                          weights_conv=conv,
+                          weights_soft=soft,
+                          learn_rate=0.01, print_acc=True)
+
+
+conv["bias_vector"]
+soft["bias_vector"]
+
--- functions.py
+++ functions.py
@@ -7,148 +7,17 @@ Created on Sun Jul 28 19:40:43 2019
 """
 # collection of functions
 
-import os
-import shutil
+#import os
+#import shutil
 import numpy as np
-import matplotlib.pyplot as plt
-
-def copy_images(source, dest, first, last):
-    """ source: directory containing original data
-        dest: target directory
-        first: number of first image to copy (e.g. '0' for 0.jpg)
-        last: number of last image to copy (e.g. '1000' for 1000.jpg)
-    """
-    fnames = [str(i) + ".jpg" for i in range(first, last)]
-    for name in fnames:
-        src = os.path.join(source, name)
-        dst = os.path.join(dest, name)
-        shutil.copyfile(src, dst)
-    return
-
-
-def extract_features(directory, datagen, conv_base, sample_count, batch_size):
-    features = np.zeros(shape=(sample_count, 4, 4, 512))
-    labels = np.zeros(shape=(sample_count))
-
-    generator = datagen.flow_from_directory(directory,
-                                        target_size=(150, 150),
-                                        class_mode="binary",
-                                        batch_size=batch_size)
-
-    i = 0
-    for inputs_batch, labels_batch in generator:
-        features_batch = conv_base.predict(inputs_batch)
-        features[i * batch_size : (i + 1) * batch_size] = features_batch
-        labels[i * batch_size : (i + 1) * batch_size] = labels_batch
-
-        i += 1
-        if i * batch_size >= sample_count:
-            break
-    return features, labels
-
-
-
-def plot_accuracy(history):
-    acc = history.history["acc"]
-    val_acc = history.history["val_acc"]
-
-    epochs = range(1, len(acc) + 1)
-
-    plt.plot(epochs, acc,  "bo", label="training acc")
-    plt.plot(epochs, val_acc,  "b", label="validation acc")
-    plt.title("training and validation accuracy")
-    plt.show()
-    return
-
-
-def plot_loss(history):
-    loss = history.history["loss"]
-    val_loss = history.history["val_loss"]
-
-    epochs = range(1, len(loss) + 1)
-
-    plt.plot(epochs, loss,  "bo", label="training loss")
-    plt.plot(epochs, val_loss,  "b", label="validation loss")
-    plt.title("training and validation loss")
-    plt.show()
-    return
-
-#### nn from scratch functions
-    
-
-def feed_forward_nn(model, data):
-    W1, b1, W2, b2 = model["W1"], model["b1"], model["W2"], model["b2"]
-    z1 = data.dot(W1) + b1
-    a1 : np.ndarray = np.tanh(z1)
-    z2 = a1.dot(W2) + b2
-    exp_scores = np.exp(z2) # for softmax
-    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) #probabilities
-    model = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
-    intermediates = {"z1": z1, "a1": a1, "z2": z2} 
-    return probs, model, intermediates
-
-
-
-def backprop_nn(probs, model, intermediates, data):
-    W1, b1, W2, b2 = model["W1"], model["b1"], model["W2"], model["b2"]
-    z1 = intermediates["z1"]
-    a1 = intermediates["a1"]
-    z2 = intermediates["z2"]
-   # Backpropagation
-    delta3 = probs
-    delta3[range(num_examples), y] -= 1
-    dW2 = (a1.T).dot(delta3)
-    db2 = np.sum(delta3, axis=0)
-    delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
-    dW1 = np.dot(data.T, delta2)
-    db1 = np.sum(delta2, axis=0)
-    
-    # update weights
-    W1 += -nu * dW1
-    b1 += -nu * db1
-    W2 += -nu * dW2
-    b2 += -nu * db2    
-
-    model["W1"], model["b1"], model["W2"], model["b2"] = W1, b1, W2, b2
-    return model
-
-
-
-
-
-def training(n_iter, data, print_acc=True, print_loss=True, mini_batch_size=1):
-    W1 = np.random.randn(nn_input_dim, h_dim)
-    b1 = np.random.randn(h_dim)
-    W2 = np.random.randn(h_dim, nn_output_dim)
-    b2 = np.random.randn(nn_output_dim)
-    
-    model = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
-    #data = X    
-    for iter in range(n_iter):
-        probs, model, intermediates = feed_forward(model, X)
-        ## stochastic gradient implementation
-        if mini_batch_size == 1:
-            model = backprop(probs, model, intermediates, data)
-        else:
-            batch_size = round(len(X) * mini_batch_size)
-            batch = np.random.choice(range(len(X)), replace=False, size=batch_size)
-            model = backprop(probs, model, intermediates, batch)    
-        if print_acc and iter % 1000 == 0:
-            predictions = np.argmax(feed_forward(model, X)[0], axis=1)
-            print(f"accuracy bei iteration {iter}", np.mean(predictions == y))
-        if print_loss and iter % 1000 == 0:
-            print(f"loss bei iteration {iter}", calculate_loss(model))
-            
-    return model
-
+#import matplotlib.pyplot as plt
 
 
 
 # convolution operation with randomnly initialized filters
-def convolute(image, number_filters):
+def convolute(image, filter_matrix, bias_vector):
     
-    filter_matrix = np.random.randn(number_filters, 3, 3) / 9
-    bias_vector = np.random.randn(number_filters) / number_filters
+    number_filters = filter_matrix.shape[0]
     height, width = image.shape
     feature_map = np.zeros(shape=(number_filters, height - 3 + 1, width - 3 + 1))
 
@@ -160,11 +29,16 @@ def convolute(image, number_filters):
     
     intermediates = {"num_fil": number_filters, "height": height,
                           "width": width}
+    
     return feature_map, intermediates
 
 
 def max_pool(feature_map):
-    number_filters, height, width = feature_map.shape
+    if len(feature_map.shape) == 3:
+        number_filters, height, width = feature_map.shape
+    else:
+        number_filters = 0
+        height, width = feature_map.shape
 #    print(feature_map.shape)
     pooling_map = np.zeros(shape=(number_filters, height // 2, width // 2))
     for k in range(number_filters):
@@ -172,24 +46,26 @@ def max_pool(feature_map):
             for j in range(width // 2):
                 res = feature_map[k, i*2:i*2 + 2, j*2:(j*2 + 2)]
                 pooling_map[k, i, j] = np.max(res)
-    
-    pooling_map_shape = pooling_map.shape
                 
-    return pooling_map, pooling_map_shape
+    return pooling_map
 
 
 
 # softmax
 
-def softmax(output_maxpool, n_classes, weight_matrix, bias_vector):
-
+def softmax(output_maxpool, weight_matrix, bias_vector):
+    
+    n_classes = weight_matrix.shape[1]
     num_filter, height, width = output_maxpool.shape
     input_len = num_filter * height * width
-    output_maxpool = np.reshape(output_maxpool, 
+    output_maxpool_flattened = np.reshape(output_maxpool, 
                                 newshape=(input_len))
     
-    input_softmax = output_maxpool.dot(weight_matrix) + bias_vector
-    exponentials = np.exp( - input_softmax)
+  #  bias_vector = np.zeros(shape=10)
+  #  weight_matrix = np.ones(shape=(1014, 10))
+    input_softmax = output_maxpool_flattened.dot(weight_matrix) + bias_vector
+    
+    exponentials = np.exp(input_softmax)
     sum_exponentials = np.sum(exponentials)
     probabilities = exponentials / sum_exponentials
     
@@ -197,79 +73,118 @@ def softmax(output_maxpool, n_classes, weight_matrix, bias_vector):
                              "sum_exp": sum_exponentials,
                              "input_softmax": input_softmax,
                              "weight_matrix": weight_matrix,
-                             "output_maxpool": output_maxpool,
+                             "output_maxpool_flattened": output_maxpool_flattened,
                              "bias_vector": bias_vector,
                              "n_classes": n_classes
                              }
     return probabilities, intermediates
 
-def backprop(inter_soft, probabilities, label, pooling_map_shape, learn_rate=0.01):
+
+def backprop(inter_soft, probabilities, label, learn_rate=0.01):
     ps = probabilities
+    
+    pooling_map_shape = inter_soft["output_maxpool_flattened"].shape
     # to implement backprop, we need intermediate results, 
     # e.g. the derivative of the loss
     
     # derivative of loss function with respect to output last layer
-    dLoss = np.zeros(inter_soft["n_classes"]) # dL / da
-    dLoss[label] = - 1 / ps[label]
+    dLoss_daL = np.zeros(inter_soft["n_classes"]) # dL / da
+    dLoss_daL[label] = - 1 / ps[label]
+
     # derivative of softmax with respect to input 
     # (input =  - (output_maxpool.dot(weight_matrix) + bias_vector) )
-    dSoft = np.zeros(inter_soft["n_classes"])
-    dSoft[label] = ((inter_soft["exp"][label] * (inter_soft["exp"][label] - inter_soft["sum_exp"])) /
-         inter_soft["sum_exp"] * inter_soft["sum_exp"])
+    #daL_dzL = np.zeros(inter_soft["n_classes"])
+    daL_dzL = ((inter_soft["exp"][label] * inter_soft["exp"]) / 
+               (inter_soft["sum_exp"] ** 2))
+    daL_dzL[label] = ((inter_soft["exp"][label] * 
+           (- inter_soft["exp"][label] + inter_soft["sum_exp"])) /
+        ( inter_soft["sum_exp"] ** 2) )
+
     # derivative of Loss with respect to bias vector in softmax
-    dbL = np.zeros(inter_soft["n_classes"])
-    dbL[label] = dSoft[label] * dLoss[label]
-    delta_L = dbL
-    dL_dbL = delta_L
+    deltaL = np.zeros(inter_soft["n_classes"])
+    deltaL[label] = dLoss_daL[label] * daL_dzL[label]
+    #dbL[label] = dLoss[label]
+    dL_dbL = deltaL
+    
     # derivative with respect to weight matrix in softmax
-    dwL = np.zeros(shape=(np.prod(pooling_map_shape), inter_soft["n_classes"]))
-    dwL[:, label] = inter_soft["output_maxpool"]
+    daL_dwL = np.zeros(shape=(np.prod(pooling_map_shape), inter_soft["n_classes"]))
+    daL_dwL[:, label] = inter_soft["output_maxpool_flattened"]
     
     # derivative of Loss function with respect to weight matrix in softmax
-    
     dL_dwL = np.zeros(shape=(np.prod(pooling_map_shape), inter_soft["n_classes"]))
-    # dL_dwL[label] = delta_L * 
-    #print(dL_dwL.shape)
-    #print(dL_dwL.shape)
-   # dL_dwL = (dwL).dot(delta_L) # shape doesn't work out
-    dL_dwL[:, label] = dwL.dot(delta_L) 
-    # dL_dwL[:, label] = 10
-    
-    #print(dL_dwL)
+    dL_dwL[:, label] = daL_dwL.dot(deltaL) 
+
     # updating weights
-    weight_matrix = inter_soft["weight_matrix"] - learn_rate * dL_dwL
+    weight_matrix = inter_soft["weight_matrix"] - learn_rate * dL_dwL 
     bias_vector = inter_soft["bias_vector"] - learn_rate * dL_dbL
     
-    return weight_matrix, bias_vector
-    
-def feed_forward(image, label, number_filters, n_classes , weight, bias, learn_rate=0.01):
-    
-    image = image / 255 - 0.5
-    out_conv, inter_conv = convolute(image=image, number_filters=number_filters)
-    out_maxpool, pooling_map_shape = max_pool(feature_map=out_conv)
-    probabilities, inter_soft = softmax( output_maxpool=out_maxpool, 
-                                                   n_classes=n_classes,
-                                                   weight_matrix=weight,
-                                                   bias_vector=bias)
-    weights, bias = backprop(inter_soft=inter_soft, probabilities=probabilities,
-             label=label, pooling_map_shape=pooling_map_shape,
-             learn_rate=learn_rate)
-    # compute cross entropy loss. Normaly cross entropy involves summing
-    # over all classes and predictions but since the true probability is 
-    # either 1 or 0 and 1 only once for every image, we dont need to sum
-    # over all classes
-    
-    loss = -np.log(probabilities[label])
-    prediction = np.argmax(probabilities)
-    acc = 1 if prediction == label else 0 #np.argmax returns indices,
-    # np.max returns value
-    
-    
-    
-    #intermediates = {"dLoss_daL": dLoss, "dSoft_dinL": dSoft, "dLoss_dwL": dwL}
-    intermediates = "bla" #
-    
-    return probabilities, loss, acc, label, prediction, intermediates, weights, bias
 
+    return weight_matrix, bias_vector
+
+
+def training(n_iter, n_classes, n_filter, training_data, label, 
+             learn_rate=0.01, print_acc=True, weights_conv=None, weights_soft=None):
+    
+    num_correct = 0
+    input_dim = int((((training_data[0].shape[0] - 3 + 1) / 2) ** 2) * n_filter)
+    
+    if weights_conv == None:
+        filter_matrix_conv = np.random.randn(n_filter, 3, 3) / 9
+        bias_vector_conv = np.random.randn(n_filter) / n_filter
+    else:
+        filter_matrix_conv = weights_conv["weight_matrix"]
+        bias_vector_conv = weights_conv["bias_vector"]
+        
+    if weights_soft == None:
+        weight_matrix_soft = np.random.randn(input_dim, n_classes) / (input_dim)
+        bias_vector_soft = np.random.randn(n_classes) / (n_classes)
+    else:
+        weight_matrix_soft = weights_soft["weight_matrix"]
+        bias_vector_soft= weights_soft["bias_vector"]
+    
+    for i in range(n_iter):
+        image = training_data[i] / 255 - 0.5
+        
+        out_conv, intermediates_conv = convolute(
+                image=image, 
+                filter_matrix=filter_matrix_conv,
+                bias_vector=bias_vector_conv)
+        
+        out_maxpool = max_pool(feature_map=out_conv)
+        
+        probabilities, intermediates_soft = softmax(
+                output_maxpool=out_maxpool, 
+                weight_matrix=weight_matrix_soft,
+                bias_vector=bias_vector_soft)
+        
+        weight_matrix_soft, bias_vector_soft = backprop(
+                probabilities=probabilities,
+                inter_soft=intermediates_soft,
+                label=label[i],
+                learn_rate=learn_rate)
+        
+            
+  #      loss = -np.log(probabilities[label])
+        prediction = np.argmax(probabilities)
+     #   print(f"prediction: {prediction}")
+     #   print(f"label: {label[i]}")
+        acc = 1 if prediction == label[i] else 0
+        num_correct += acc
+        
+        if i % 100 == 0 and i != 0 and print_acc:
+            accuracy = num_correct / i
+            print(f"accuracy for the first {i} samples: {accuracy}")
+            print(f"{num_correct} predictions for {i} samples were correct")
+      #      print(bias_vector_conv)
+       #     print(bias_vector_soft)
+            
+    weights_conv = {"weight_matrix": filter_matrix_conv, 
+                    "bias_vector": bias_vector_conv}
+    
+    weights_soft = {"weight_matrix": weight_matrix_soft,
+                    "bias_vector": bias_vector_soft}
+
+    return weights_conv, weights_soft
+    
 
 
