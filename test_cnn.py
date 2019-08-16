ee0ea5981e6ef65e8889687190adda503d9f177f
Karl-M <djdanger@gmx.de>
2019-08-16 19:30:02 +0200

    testfunction rewritten with better names

--- test_cnn.py
+++ test_cnn.py
@@ -34,12 +34,12 @@ from softmax import Softmax
 
 def debug_cnn(n_iter):
     num_filters = 8
-    np.random.seed(seed=666); filter_conv = np.random.randn(num_filters, 3, 3) * 3# / 9
-    filter_conv = np.round(filter_conv)
+    np.random.seed(seed=666); own_filter_conv = np.random.randn(num_filters, 3, 3) * 3# / 9
+    own_filter_conv = np.round(own_filter_conv)
     
     dim_maxpool = 13 * 13 *8
-    np.random.seed(seed=666); weight_soft = (np.random.randn(dim_maxpool, 10) / dim_maxpool) * 10
-    bias_soft = np.zeros(10)
+    np.random.seed(seed=666); own_weight_soft = (np.random.randn(dim_maxpool, 10) / dim_maxpool) * 10
+    own_bias_soft = np.zeros(10)
     
     conv = Conv3x3(8)  # 28x28x1 -> 26x26x8
     pool = MaxPool2()  # 26x26x8 -> 13x13x8
@@ -50,40 +50,42 @@ def debug_cnn(n_iter):
 
         image = test_images[i] / 255 - 0.5
         label = test_labels[i]
-        feature_map, filter_conv = fun.convolute(image=image, filter_matrix=filter_conv)
-        maxpool_map = fun.maxpool(feature_map=feature_map)
-        probs, inter_soft = fun.softmax(maxpool_map, 
-                                        weight_matrix=weight_soft, 
-                                        bias_vector=bias_soft)
-        weight_soft, bias_soft, gradient_soft_own = fun.backprop_softmax(inter_soft=inter_soft,
-                                                           probabilities=probs,
+        
+        own_feature_map, own_filter_conv = fun.convolute(image=image, filter_matrix=own_filter_conv)
+        own_maxpool_map = fun.maxpool(feature_map=own_feature_map)
+        own_probs, own_inter_soft = fun.softmax(own_maxpool_map, 
+                                        weight_matrix=own_weight_soft, 
+                                        bias_vector=own_bias_soft)
+        own_weight_soft, own_bias_soft, own_gradient_soft = fun.backprop_softmax(inter_soft=own_inter_soft,
+                                                           probabilities=own_probs,
                                                            label = label,
                                                            learn_rate=0.1) 
-        gradient_max_own = fun.backprop_maxpool(feature_map=feature_map, 
-                                                gradient=gradient_soft_own)
-        filter_conv = fun.backprop_conv(image=image, filter_conv=filter_conv,
-                                        gradient=gradient_max_own, learn_rate=0.1)
+        own_gradient_max = fun.backprop_maxpool(feature_map=own_feature_map, 
+                                                gradient=own_gradient_soft)
+        own_filter_conv = fun.backprop_conv(image=image, filter_conv=own_filter_conv,
+                                        gradient=own_gradient_max, learn_rate=0.1)
      
        
     # run model from blog with same data
 
-        out_conv = conv.forward(image)
+        blog_out_conv = conv.forward(image)
         #print(out_conv)   
-        out_max = pool.forward(out_conv)
-        out_soft, weights, summe = softmax.forward(out_max) #
+        blog_out_max = pool.forward(blog_out_conv)
+        blog_out_soft, blog_weights, blog_summe = softmax.forward(blog_out_max) #
         #    print(out_soft)
         gradient_L = np.zeros(10)
-        gradient_L[label] = -1 / out_soft[label]
-        gradient_soft, dL_dw, weights_updated, biases_updated = softmax.backprop(gradient_L, 0.1)
-        gradient_max = pool.backprop(gradient_soft)
-        gradient_conv, filter_update = conv.backprop(gradient_max, 0.1)
+        gradient_L[label] = -1 / blog_out_soft[label]
+        blog_gradient_soft, blog_dL_dw, blog_weights_updated, blog_biases_updated = softmax.backprop(
+                gradient_L, 0.1)
+        blog_gradient_max = pool.backprop(blog_gradient_soft)
+        blog_gradient_conv, blog_filter_update = conv.backprop(blog_gradient_max, 0.1)
         
         
         
         ################## compare feedforward ####################################
         ###########################################################################
         print("This is iteration", i)
-        if np.sum(feature_map == out_conv) == np.prod(feature_map.shape):
+        if np.sum(own_feature_map == blog_out_conv) == np.prod(own_feature_map.shape):
             print("Yeaaah! FeatureMaps are the same")
         else:
             print("featuremaps are not the same")
@@ -91,11 +93,11 @@ def debug_cnn(n_iter):
        # conv.filters == filter_conv # 
         # after first iteration these are not the same anymore,
         # since they get updated
-        if np.sum(maxpool_map == out_max) == np.prod(out_max.shape):
+        if np.sum(own_maxpool_map == blog_out_max) == np.prod(blog_out_max.shape):
             print("Yeaaah! maxpool is the same")
         else:
             print("maxpool is not the same")
-        if np.sum(probs == out_soft) == np.prod(out_soft.shape):
+        if np.sum(own_probs == blog_out_soft) == np.prod(blog_out_soft.shape):
             print("Yeaaah! predicted probabilities are the same")
         else:
             print("predicted probabilities are not the same")
@@ -104,27 +106,27 @@ def debug_cnn(n_iter):
         ############################################################################
          
         ######## softmax: gradients:    
-        if np.sum(gradient_soft == gradient_soft) == np.prod(gradient_soft.shape):
+        if np.sum(own_gradient_soft == blog_gradient_soft) == np.prod(blog_gradient_soft.shape):
             print("Yeaaah! gradients softmax are the same")
         else:
             print("gradients softmax are not the same")
         ## weight updates weight matrix softmax layer
-        if np.sum(weight_soft == weights_updated) == np.prod(weight_soft.shape):
+        if np.sum(own_weight_soft == blog_weights_updated) == np.prod(own_weight_soft.shape):
             print("Yeaaah! updated weightmatrix softmax is the same")
         else:
             print("updated weightmatrix softmax is not the same")
         ## weight updates bias vector
-        if np.sum(bias_soft == biases_updated) == np.prod(biases_updated.shape):
+        if np.sum(own_bias_soft == blog_biases_updated) == np.prod(blog_biases_updated.shape):
             print("Yeaaah! Updated bias vector softmax is the same")
         else:
             print("updated bias vector is not the same")
         #### maxpool
-        if np.sum(gradient_max_own== gradient_max) == np.prod(gradient_max.shape):
+        if np.sum(own_gradient_max== blog_gradient_max) == np.prod(blog_gradient_max.shape):
             print("Yeaaah! gradients maxpool layer are the same")
         else:
             print("updated gradients maxpool are not the same")
         ## conv
-        if np.sum(filter_conv == filter_update) == np.prod(filter_conv.shape):
+        if np.sum(own_filter_conv == blog_filter_update) == np.prod(own_filter_conv.shape):
             print("Yeaaah! updated filter convlayer are the same")
         else:
             print("updated filter conv layer is not the same")
